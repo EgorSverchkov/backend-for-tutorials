e7ba0b05941801cb2350f9038036b8b8b4607ebf
Egor Sverchkov <sverch322@gmail.com>
2024-01-10 13:51:30 +0300

    add feedback api

--- src/main/java/ru/sverchkov/backendfortutorials/exception/EmptyRequestException.java
+++ src/main/java/ru/sverchkov/backendfortutorials/exception/EmptyRequestException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class EmptyTutorialRequestException extends RuntimeException {
-    public EmptyTutorialRequestException(String message) {
+public class EmptyRequestException extends RuntimeException {
+    public EmptyRequestException(String message) {
         super(message);
     }
 }
